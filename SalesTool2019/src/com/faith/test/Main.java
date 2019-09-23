70dae052cbab2961f2d58af2f044b9121057814e
Shilpa Pillai <Shilpa Pillai@HP-PAVILION>
2019-09-23 15:14:37 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,17 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		SalesData obj=new SalesData();
+		
+		System.out.println("Hello UST Guys! Git Repo");
+		displayGreetings();
+		obj.display();
+	}
+	
+	private static void displayGreetings(){
+		System.out.println(("Welcome to Sales Report App 2019"));
+	}
+}
